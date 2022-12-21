aaa4628de39e563a50e8246e47bafc684c388b5f
atcupps <91998663+atcupps@users.noreply.github.com>
2022-12-20 23:02:18 -0500

    Added genetic constructor to Edge

--- src/neural/Edge.java
+++ src/neural/Edge.java
@@ -1,5 +1,7 @@
 package neural;
 
+import functions.Function;
+
 /**
  * Edges connect Nodes to other Nodes; an Edge stores the destination of the
  * edge in the next field, as well as the weight and bias of the Edge; Edges
@@ -25,11 +27,35 @@ public class Edge {
 	/**
 	 * The default constructor for creating a new Edge to a Node; by default, 
 	 * weight and bias are random numbers between -1 and 1. 
-	 * @param nextNode
+	 * @param nextNode - the destination Node of the Edge
 	 */
 	public Edge(Node nextNode) {
 		next = nextNode;
 		weight = Math.random() * 2 - 1;
 		bias = Math.random() * 2 - 1;
+	}
+	
+	/**
+	 * Genetic algorithmic inheritance constructor for an Edge in a
+	 * Network being inherited. Data is copied from toInherit, with a maximum
+	 * variation of plus or minus maxVariance.
+	 * @param nextNode - the destination Node of the Edge
+	 * @param toInherit - the Edge to be inherited
+	 * @param maxVariance - the maximum variance of inherited Edge data
+	 */
+	public Edge(Node nextNode, Edge toInherit, double maxVariance) {
+		next = nextNode;
+		weight = toInherit.weight + Math.random() * maxVariance * 2 - maxVariance;
+		bias = toInherit.bias + Math.random() * maxVariance * 2 - maxVariance;
+	}
+	
+	/**
+	 * Adds the parameter data to the current data of the destination Node of
+	 * this edge after processing it through the sigmoid function with the
+	 * Edge's weight and bias.
+	 * @param data - the data to be transferred to the next Node.
+	 */
+	public void addData(double data) {
+		next.addData(Function.sigmoid(data, weight, 1, bias, 0));
 	}
 }
