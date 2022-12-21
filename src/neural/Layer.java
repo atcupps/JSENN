f96122c607852450c99fc487679d54cb03d1b475
atcupps <91998663+atcupps@users.noreply.github.com>
2022-12-20 23:02:57 -0500

    Added genetic constructors for Layer

--- src/neural/Layer.java
+++ src/neural/Layer.java
@@ -48,6 +48,24 @@ public class Layer {
 			nodes[i] = new Node();
 		}
 	}
+
+	/**
+	 * Genetic algorithmic inheritance constructor for an output Layer. Copies the
+	 * data from the parameter Layer with slight variations at a maximum of
+	 * maxVariance.
+	 * @param toInherit - the Layer to be copied
+	 * @param maxVariance - the maximum variance of Layer data
+	 */
+	public Layer(Layer toInherit, double maxVariance) {
+		isOutput = true;
+		
+		this.size = toInherit.size;
+		nodes = new Node[size];
+		
+		for (int i = 0; i < nodes.length; i++) {
+			nodes[i] = new Node(toInherit.nodes[i], maxVariance);
+		}
+	}
 	
 	/**
 	 * Constructor for a non-output layer with unspecified Node identifications
@@ -71,6 +89,27 @@ public class Layer {
 			for (int j = 0; j < nextLayerNodes.length; j++) {
 				nodes[i].createEdge(nextLayerNodes[j]);
 			}
+		}
+	}
+	
+	/**
+	 * Genetic algorithmic inheritence constructor for a new non-output Layer
+	 * copied from a corresponding non-output Layer in another Network. Data
+	 * is copied with maxVariance amount of variance (+/- maxVariance).
+	 * @param toInherit - the Layer to be inherited
+	 * @param next - the next Layer in the Network
+	 * @param maxVariance - the maximum variance of copied data
+	 */
+	public Layer(Layer toInherit, Layer next, double maxVariance) {
+		isOutput = false;
+		
+		this.size = toInherit.size;
+		nodes = new Node[size];
+		
+		this.next = next;
+		
+		for (int i = 0; i < nodes.length; i++) {
+			nodes[i] = new Node(toInherit.nodes[i], next.nodes, maxVariance);
 		}
 	}
 
@@ -98,6 +137,32 @@ public class Layer {
 		}
 		
 		return null;
+	}
+	
+	/**
+	 * Resets all the Nodes of the current Layer to have their default data values.
+	 */
+	public void resetLayer() {
+		for (Node n : nodes) {
+			n.resetData();
+		}
+	}
+	
+	/**
+	 * Returns false if this Layer is an output layer; otherwise, this method
+	 * will call all Nodes to transfer their data, then returns true.
+	 * @return false if isOutput, true if not
+	 */
+	public boolean transferData() {
+		if (isOutput) {
+			return false;
+		}
+		
+		for (Node n : nodes) {
+			n.transferData();
+		}
+		
+		return true;
 	}
 	
 }
